47bd31aedc41db2c1d479b46d3bdcad948f4f03e
Gregory Paone <gppaone@ncsu.edu>
2024-09-11 20:18:16 -0400

    logo change

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -4,7 +4,7 @@
 <nav class="navbar">
     <div class="container">
         <a class="navbar-brand" href="/">
-            <img src="/images/test.png" alt="strumspace" height="75" />
+            <img src="/images/logo.png" alt="strumspace" height="75" />
         </a>
     </div>
 </nav>
